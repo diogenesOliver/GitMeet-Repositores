e37da9f81483f616f56a7a5bcd43ea332b1fa6c6
Diógenes Ribeiro de Oliveira <91908375+diogenesOliver@users.noreply.github.com>
2023-04-21 16:25:04 +0000

    feat: route/repositories

--- gitmeet-repo/src/App.tsx
+++ gitmeet-repo/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import { MainPage } from './pages/MainPage';
+import { Repositories } from './pages/Repositories';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<MainPage/>}/>
-        <Route path='/repositories' element={<MainPage/>}/>
+        <Route path='/repositories' element={<Repositories/>}/>
       </Routes>
     </BrowserRouter>
   );
