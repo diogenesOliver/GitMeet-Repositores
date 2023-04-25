72a15675a17672e4a9a2cbe7c61bae3e65915c11
Diógenes Ribeiro de Oliveira <91908375+diogenesOliver@users.noreply.github.com>
2023-04-25 00:58:29 +0000

    feat: Styling bton back to home page

--- gitmeet-repo/src/components/ButtonToBack.tsx
+++ gitmeet-repo/src/components/ButtonToBack.tsx
@@ -1,4 +1,7 @@
-import { useNavigate } from 'react-router-dom'
+import '../style/ButtonBackToHomePage.css';
+
+import { useNavigate } from 'react-router-dom';
+import img from '../assets/icons8-back-50.png';
 
 export function ButtonToBackFromHomePageComponent() {
     const navigate = useNavigate()
@@ -7,11 +10,14 @@ export function ButtonToBackFromHomePageComponent() {
     }
 
     return (
-        <button
+        <>
+            <button
             onClick={goToHomePage}
-            className='button-back'
-        >
-            Back
-        </button>
+                className='button-back'
+                >
+                <img src={img} alt="Icon back to home page" />
+                <p>Back to home page...</p>
+            </button>
+        </>
     )
 }
